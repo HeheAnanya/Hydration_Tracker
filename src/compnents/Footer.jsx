dfd04fef98cf4df6e4ddd03675d6a1110b89d5d8
HeheAnanya <ananyanarang1411@gmail.com>
2025-05-06 17:06:57 +0530

    made changes in footer

--- src/compnents/Footer.jsx
+++ src/compnents/Footer.jsx
@@ -1,39 +1,3 @@
-// import React from 'react'
-// import './Footer.css'
-// import logo from '../assets/logo.png'
-
-// const Footer = () => {
-//   return (
-//     <>
-//         <div className='footer'>
-//             <div>
-//             <img src={logo} className='footerLogo'/>
-//             <h2 className='companyName'>Hydra</h2>
-//             </div>
-//             <div className='nav'>
-//                 <div className='footerTopic'>
-//                <h3>Quick Links</h3>
-//                 <ul>
-//                         <li><a href="#">About Us</a></li>
-//                         <li><a href="#">Blog</a></li>
-//                         <li><a href="#" >Terms of Service</a></li>
-//                     </ul>
-//                 </div>
-
-
-
-//         </div>
-
-//     </div>
-//     </>
-
-//   )
-// }
-
-// export default Footer
-
-
-
 import React from 'react';
 import './Footer.css';
 const Footer = () => {
@@ -69,3 +33,55 @@ const Footer = () => {
 };
 
 export default Footer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import './Footer.css'
+// import logo from '../assets/logo.png'
+
+// const Footer = () => {
+//   return (
+//     <>
+//         <div className='footer'>
+//             <div>
+//             <img src={logo} className='footerLogo'/>
+//             <h2 className='companyName'>Hydra</h2>
+//             </div>
+//             <div className='nav'>
+//                 <div className='footerTopic'>
+//                <h3>Quick Links</h3>
+//                 <ul>
+//                         <li><a href="#">About Us</a></li>
+//                         <li><a href="#">Blog</a></li>
+//                         <li><a href="#" >Terms of Service</a></li>
+//                     </ul>
+//                 </div>
+
+
+
+//         </div>
+
+//     </div>
+//     </>
+
+//   )
+// }
+
+// export default Footer
